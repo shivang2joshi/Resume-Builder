79c1aa6f1b0f689cb22d0f11be6c2227b29efccf
Master Shivang Joshi <shivang.g.joshi@gmail.com>
2019-03-22 01:25:38 +0530

    added SignInWithGoogle and Logout

---
+++ app.js
@@ -0,0 +1,41 @@
+
+firebase.auth().onAuthStateChanged(function (user) {
+    // caution! this will run constantly!
+    // i tried putting redirection here it is conatantly redirecting
+    if (user) {
+        //signed in
+    } else {
+        //not signed in
+    }
+});
+
+function login() {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function (result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        window.location = "templates.html";
+        document.getElementById('user-photo').src=user.photoURL;
+        document.getElementById('user-name').innerHTML=user.displayName;     
+        
+        // ...
+    }).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+    });
+};
+
+function logout() {
+    firebase.auth().signOut()
+        .then(function () {
+            window.location = "index.html";
+        });
+}
