0f2211cf3bd502f46c04d6541b9585b36c0d7daa
Master Shivang Joshi <shivang.g.joshi@gmail.com>
2019-04-13 18:18:00 +0530

    login bypassed for testing purpose

--- functions.js
+++ functions.js
@@ -1,11 +1,11 @@
 function login() {
-    var provider = new firebase.auth.GoogleAuthProvider();
+    /*var provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth().signInWithPopup(provider).then(function (result) {
         // This gives you a Google Access Token. You can use it to access the Google API.
         var token = result.credential.accessToken;
         // The signed-in user info.
         var user = result.user;
-        window.location = "dashboard.html";
+        
 
     }).catch(function (error) {
         // Handle Errors here.
@@ -17,7 +17,8 @@ function login() {
         var credential = error.credential;
         // ...
         window.alert(errorMessage);
-    });
+    });*/
+    window.location = "dashboard.html";
 };
 
 function logout() {
